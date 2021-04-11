ea2ee2840bfc7881271acf019182f1d91f1fee04
Rajesh Kumar <rajeshthiagu99@gmail.com>
2021-04-11 12:32:41 +0530

    tut

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
 import flask
 from flask import request
+import pandas as pd
+from datetime import datetime as dt
+
+data = pd.read_csv('data.csv', names=['s', 'e', 'm']).set_index('m')
+
+series = pd.Series(index=range(data.s.min(), dt.now().year + 1))
+for m in data.index:
+    series.loc[data.loc[m].s:data.loc[m].e] = m
 
 app = flask.Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return 'res'
-
-
-
-
-
-# @app.route('/', methods=['GET'])
-# def home():
-    # res = request.json
-    
-    # print(res)
-    # print(res.json())
-    # return 'res'
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-
+    year = int(request.args['year'])
+    try:
+        return series.loc[year]
+    except KeyError:
+        return f'Invalid input ({series.index.min()} - {series.index.max()})'
