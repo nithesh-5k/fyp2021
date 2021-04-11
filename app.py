ac46b78b1e5268924fc172faf215b98dddecbed1
Rajesh Kumar <rajeshthiagu99@gmail.com>
2021-04-11 13:33:12 +0530

    finalads

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 from flask import request
 from datetime import datetime as dt
 import json
+import pandas as pd
 
 app = flask.Flask(__name__)
 
@@ -9,7 +10,7 @@ app = flask.Flask(__name__)
 @app.route('/', methods=['POST'])
 def home():
     res = request.json
+    data=pd.DataFrame(res['distances'][0])
 
-
-    return res['distances'][0]
+    return {'data': data}
     
