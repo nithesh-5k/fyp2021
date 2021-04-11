b4d18eeed0b04d1f089e6a4e8a5ed8a4f3440f15
Rajesh Kumar <rajeshthiagu99@gmail.com>
2021-04-11 12:27:03 +0530

    get

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import flask
-from flask import Flask, request, jsonify
+from flask import request
 
 app = flask.Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def home():
+    return 'res'
+
+
+
+
+
+# @app.route('/', methods=['GET'])
+# def home():
     # res = request.json
     
     # print(res)
     # print(res.json())
-    return 'res'
+    # return 'res'
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
